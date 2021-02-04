7407713dc744f85225ff922d8cb7c3e005ce0de4
ariana <arinakhotinskaya@mail.ru>
2021-02-04 22:28:09 +0300

    Initial commit

--- 6.4.py
+++ 6.4.py
@@ -11,3 +11,70 @@
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 '''
+
+class Car:
+    def __init__(self, speed, color, name, is_police = False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print(f'Машина под названием {self.name} поехала')
+
+    def stop(self):
+        print(f'Машина под названием {self.name} остановилась')
+
+    def turn(self, direction):
+        self.direction = direction
+        print(f'Машина под названием {self.name} повернула {self.direction}')
+
+    def show_speed(self):
+        print(f'Машина имеет скорость {self.speed}')
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            print(f'Вы превышаете скорость на {self.speed - 60}. Сбавьте!')
+        else:
+            print(f'У вас нормальная скорость!')
+
+class SportCar(Car):
+    pass
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            print(f'Вы превышаете скорость на {self.speed - 40}. Сбавьте!')
+        else:
+            print(f'У вас нормальная скорость!')
+
+class PoliceCar(Car):
+    pass
+
+town = TownCar(90, 'зеленый', 'BMW')
+town.go()
+town.show_speed()
+town.turn("направо")
+town.stop()
+print('\n')
+
+sport = SportCar(110, 'белый', 'Maserati')
+sport.go()
+sport.show_speed()
+sport.turn("налево")
+sport.stop()
+print('\n')
+
+work = WorkCar(50, 'серый', 'WAZ')
+work.go()
+work.show_speed()
+work.turn("назад")
+work.stop()
+print('\n')
+
+police = PoliceCar(80, 'белый', 'Mercedes-Benz', True)
+police.go()
+police.show_speed()
+police.turn("направо")
+police.stop()
